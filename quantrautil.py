e71d58ceee7cd3a0d47072da030b450e35e81000
ishanshah15 <40386408+ishanshah15@users.noreply.github.com>
2019-01-18 17:31:53 +0530

    Update quantrautil.py

--- quantrautil.py
+++ quantrautil.py
@@ -9,17 +9,17 @@ import nsepy
 from quandl.errors.quandl_error import AuthenticationError
 
 def get_quantinsti_api_key():
-    '''
+    """
         This function returns the Quandl's API key which is used to access quandl data
         To get your API key, sign up for a free Quandl account
         Then, you can find your API key on Quandl account settings page
-    '''
+    """
     return '<<Copy Paste your Quandl API Key here>>'
 
 def get_data(ticker, start_date='2016-01-01', end_date='2017-01-01'):
-    '''
+    """
         This function fetches the data from different web source such as Quandl, Yahoo finance and NSEPy
-    '''
+    """
     try:
         df = quandl.get('WIKI/'+ticker, start_date=start_date, end_date=end_date, api_key=get_quantinsti_api_key())
         df['Source'] = 'Quandl Wiki'
